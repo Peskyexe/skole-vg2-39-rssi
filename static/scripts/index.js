020408ad02b1281c0e902330289aa96b6aee2834
Peskyexe <tthdarklama08@gmail.com>
2026-09-25 11:00:33 +0200

    Ryddet koden og la til kommentarer

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,73 +1,98 @@
-import { setIndicatorPosition } from "./position-display.js";
+import { updateIndicators } from "./position-display.js";
 
-const distanceElement = document.getElementById("distance");
-const distanceAvgElement = document.getElementById("distance-avg");
 
 function rssiToDistance(rssi) {
     const RSSI_1M = -29;
-    const PATH_LOSS = 2.1;
+    const PATH_LOSS = 2.05;
     return 10 ** ((RSSI_1M - (-1 * rssi)) / (10 * PATH_LOSS));
 }
 
-const rssiElement = document.getElementById("rssi");
-const rssiAvgElement = document.getElementById("rssi-avg");
+export { rssiToDistance };
 
-const rssiValues = [];
-const rssiMaxHistory = 6;
-const minmaxStableThreshold = 2;
+
+const rssiHistory = [];
+const maxHistory = 6;
 
 async function getRSSI() {
     try {
+        // Bruker Python RSSI API-en
         const response = await fetch("/rssi");
         const data = await response.json();
 
         const rssi = data.rssi
 
-        rssiValues.push(rssi);
-        if (rssiValues.length > rssiMaxHistory) {
-            rssiValues.shift();
+        // Lagrer en kort historie av RSSI-er
+        rssiHistory.push(rssi);
+        if (rssiHistory.length > maxHistory) {
+            rssiHistory.shift();
         }
 
         return rssi;
     } catch (error) {
-        return null;
+        console.error(error)
     }
 }
 
+
+const avgRssiHistory = [];
+
 function getAverageRSSI() {
-    const rssiAvg = rssiValues.reduce((sum, value) => sum + value, 0) / rssiValues.length;
-    return rssiAvg.toFixed(1);
+    // Regner ut en gjennomsnittling RSSI fra historien 
+    const avgRssi = rssiHistory.reduce((sum, value) => sum + value, 0) / rssiHistory.length;
+
+    avgRssiHistory.push(avgRssi);
+    if (avgRssiHistory.length > maxHistory) {
+        avgRssiHistory.shift()
+    }
+
+    return avgRssi.toFixed(1);
 }
 
-async function updateRSSI() {
+
+const minmaxStableThreshold = 3;
+
+function checkIfSignalStable(rssiHistory) {
+    // Regner ut forskjellen mellom den laveste RSSI-en og den høyeste RSSI-en fra historien
+    const max = Math.max(...rssiHistory);
+    const min = Math.min(...rssiHistory);
+    const minmaxDifference = max - min;
+
+    // Hvis forskjellen er mindre enn en terskel så regner vi med at signalet er stabilt 
+    if (minmaxDifference <= minmaxStableThreshold) {
+        rssiAvgElement.classList.add("stable");
+        distanceAvgElement.classList.add("stable");
+    } else {
+        rssiAvgElement.classList.remove("stable");
+        distanceAvgElement.classList.remove("stable");
+    }
+}
+
+
+const rssiElement = document.getElementById("rssi");
+const rssiAvgElement = document.getElementById("rssi-avg");
+const distanceElement = document.getElementById("distance");
+const distanceAvgElement = document.getElementById("distance-avg");
+
+// Oppdatere alle elementer og visninger.
+async function updateAll() {
     const rssi = await getRSSI();
 
     if (rssi !== null) {
-        const avgRSSI = getAverageRSSI();
+        rssiElement.textContent = `-${rssi}`;
 
-        rssiElement.textContent = rssi;
-        rssiAvgElement.textContent = avgRSSI;
-
-        const max = Math.max(...rssiValues);
-        const min = Math.min(...rssiValues);
-        const minmaxGap = max - min;
-
-        if (minmaxGap <= minmaxStableThreshold) {
-            rssiAvgElement.classList.add("stable");
-            distanceAvgElement.classList.add("stable");
-        } else {
-            rssiAvgElement.classList.remove("stable");
-            distanceAvgElement.classList.remove("stable");
-        }
+        const avgRssi = getAverageRSSI();
+        rssiAvgElement.textContent = `-${avgRssi}`;
 
         const distance = rssiToDistance(rssi);
         distanceElement.textContent = distance.toFixed(1);
 
-        const distanceAvg = rssiToDistance(avgRSSI);
+        const distanceAvg = rssiToDistance(avgRssi);
         distanceAvgElement.textContent = distanceAvg.toFixed(1);
 
-        setIndicatorPosition(distanceAvg);
+        checkIfSignalStable(rssiHistory);
+        updateIndicators(avgRssiHistory);
     } else if (rssi === null) {
+        // Hvis vi ikke mottar noe RSSI verdi fra serveren så resetter vi alle tekst elementene.
         distanceElement.textContent = "--";
         distanceAvgElement.textContent = "--";
         rssiElement.textContent = "--";
@@ -75,5 +100,5 @@ async function updateRSSI() {
     }
 }
 
-updateRSSI();
-setInterval(updateRSSI, 1000);
+updateAll();
+setInterval(updateAll, 1000);
