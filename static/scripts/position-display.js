82c7533b9bd3508855ad723c068d28cbf774e72f
Peskyexe <tthdarklama08@gmail.com>
2026-09-25 10:59:57 +0200

    La til flere indikatorer som viser historikk på en visuell måte. I tillegg så ryddet koden og la til kommentarer.

--- static/scripts/position-display.js
+++ static/scripts/position-display.js
@@ -1,19 +1,42 @@
+import { rssiToDistance } from './index.js';
+
+
 const displayElement = document.getElementById("position-display");
-const indicatorElement = document.getElementById("position-indicator");
 
-const displayMaxMeters = 10;
-const displayMinMeters = 0;
-const displayWidthPixels = 750;
+const displayMaxMeters = displayElement.dataset.maxMeters;
+const displayMinMeters = displayElement.dataset.minMeters;
 
+const displayWidthPixels = displayElement.dataset.widthPx;
 displayElement.style.setProperty('--width', `${displayWidthPixels}px`);
 
-function metersToPixels(meters) {
-	return ((meters - displayMinMeters) / (displayMaxMeters - displayMinMeters)) * displayWidthPixels;
+function metersToPixelsBounded(meters) {
+    // Passer på at meter verdien ikke er mer- eller mindre en min og max verdien til displayet
+    const boundedMeters = Math.min(Math.max(meters, displayMinMeters), displayMaxMeters);
+
+    // Konverterer meter til pixler 
+	return ((boundedMeters - displayMinMeters) / (displayMaxMeters - displayMinMeters)) * displayWidthPixels;
 }
 
-function setIndicatorPosition(positionInMeters) {
-    const positionInPixels = metersToPixels(positionInMeters);
-    indicatorElement.style.setProperty('--position', `${positionInPixels}px`);
+
+// Henter inn alle indikatorene fra posisjon displayet. 
+const indicators = document.querySelectorAll('.position-indicator');
+
+// Oppdaterer alle indikatorene sin posisjon 
+function updateIndicators(rssiHistory) {
+    const setIndicatorPosition = (indicator, distance) => {
+        const position = metersToPixelsBounded(distance);
+        indicator.style.setProperty('--position', `${position}px`);
+    }
+
+    // De nyeste RSSI oppdateringene ligger bakerst, så vi flipper historien for å få de nyeste først.
+    // Istedet for Eldste -> Nyeste så blir det Nyeste -> Eldste
+    const flippedRssiHistory = rssiHistory.toReversed();
+
+    indicators.forEach(indicator => {
+        const rssi = flippedRssiHistory.shift();
+        const distance = rssiToDistance(rssi);
+        setIndicatorPosition(indicator, distance)
+    });
 }
 
-export { setIndicatorPosition }
+export { updateIndicators }
